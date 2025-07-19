a3f3844cd891d23d326f5df23ce2f867c6c7938a
Dhileeban <berlin4evr@icloud.com>
2025-07-20 01:40:24 +0530

    Method Hiding

---
+++ MethodHiding/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace MethodHiding
+{
+    public class Employee
+    {
+        public string firstName;
+        public string lastName;
+
+        public void PrintFullName()
+        {
+            Console.WriteLine($"Full Name: {firstName} {lastName}");
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        // Method hiding using new keyword
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        public new void PrintFullName()
+        {
+            Console.WriteLine($"Full Name: {firstName} {lastName} - PartTime Employee");
+            base.PrintFullName(); // Call the base class method
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FullTimeEmployee fullTimeEmployee = new FullTimeEmployee();
+            fullTimeEmployee.firstName = "Dhileeban";
+            fullTimeEmployee.lastName = "M";
+            fullTimeEmployee.PrintFullName();
+
+            PartTimeEmployee partTimeEmployee = new PartTimeEmployee();
+            partTimeEmployee.firstName = "Berlin";
+            partTimeEmployee.lastName = "M";
+            partTimeEmployee.PrintFullName();
+        }
+    }
+}
