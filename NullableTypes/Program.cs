2a0869ffe91342a1ea0e773512571289f643a3ba
Dhileeban <berlin4evr@icloud.com>
2025-07-20 00:29:56 +0530

    Nullable Types

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace NullableTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Value types can be null using nullable types
+            int? nullableInt = null; // Nullable integer
+            double? nullableDouble = null; // Nullable double
+            Console.WriteLine("Nullable Integer: " + nullableInt);
+            Console.WriteLine("Nullable Double: " + nullableDouble);
+
+
+            /* ------- Null -coalescing operator Example------ */
+            // The null-coalescing operator (??) provides a default value if the left operand is null
+            int? TicketsOnSale = null; // Nullable integer for tickets on sale
+            int defaultTickets = 100; // Default number of tickets
+            // Using null-coalescing operator to provide a default value if TicketsOnSale is null
+            int ticketsToDisplay = TicketsOnSale ?? defaultTickets; // If TicketsOnSale is null, use defaultTickets
+            Console.WriteLine("Tickets on Sale: " + ticketsToDisplay);
+        }
+    }
+}
