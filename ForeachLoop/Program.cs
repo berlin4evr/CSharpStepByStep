41fd3db7db1634da92008dd6212f8d1573913077
Dhileeban <berlin4evr@icloud.com>
2025-07-20 01:08:06 +0530

    Foreach Loop

---
+++ ForeachLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 10;
+            numbers[1] = 20;
+            numbers[2] = 30;
+
+            // Using foreach loop to iterate through the array
+            foreach (int n in numbers)
+            {
+                Console.WriteLine("The value of number is {0}", n);
+            }
+
+            // Alternatively, using a foreach loop with an index
+            int index = 0;
+            foreach (int i in numbers)
+            {
+                Console.WriteLine("The value of number[{0}] is {1}", index, i);
+                index++;
+            }
+        }
+    }
+}
