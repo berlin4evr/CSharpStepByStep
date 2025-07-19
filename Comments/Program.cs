12883b34a8d578405ac1d2f57b1b49b9bfff84a8
Dhileeban <berlin4evr@icloud.com>
2025-07-20 00:42:36 +0530

    Comments - Differnt types of comments explained

---
+++ Comments/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Comments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// XML comments are used to provide documentation for the code.
+            /// </summary>
+            /// <param name="args"></param>
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Hello, World!"); // single-line comment explaining the purpose of the program
+
+                /*
+                 * This is a multi-line comment.
+                 * It can span multiple lines and is used to provide detailed explanations
+                 * or documentation about the code.
+                 */
+            }
+        }
+    }
+}
