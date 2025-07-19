51c7532d5ede651c109e77145591fa602808fbef
Dhileeban <berlin4evr@icloud.com>
2025-07-20 00:44:58 +0530

    IF Statement explained

---
+++ IfStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace IfStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            int x = number;
+
+            // If statement to check if the number is greater than 5
+            if (number > 5)
+            {
+                Console.WriteLine($"{number} is greater than 5");
+            }
+            else if (number < 5)
+            {
+                Console.WriteLine($"{number} is less than 5");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is equal to 5");
+            }
+        }
+    }
+}
