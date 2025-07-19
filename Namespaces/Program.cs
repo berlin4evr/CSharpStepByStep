2b7d9906c8c7e4cd2244bf39953a7177c26db0e4
Dhileeban <berlin4evr@icloud.com>
2025-07-20 01:25:27 +0530

    Namspaces

---
+++ Namespaces/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using PATA = ProjectA.TeamA; // Importing the namespace for ClassA in TeamA
+using PATB = ProjectA.TeamB; // Importing the namespace for ClassA in TeamB
+using System.IO;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        PATA.ClassA.DisplayMessage(); // Call the static method from ClassA in ProjectA.TeamA
+        PATB.ClassA.DisplayMessage(); // Call the static method from ClassA in ProjectA.TeamB
+
+    }
+}
+
+
+namespace ProjectA
+{
+    namespace TeamA
+    {
+        public class ClassA
+        {
+            public static void DisplayMessage()
+            {
+                Console.WriteLine("Hello from ClassA in ProjectA.Team-A");
+            }
+        }
+    }
+}
+
+
+namespace ProjectA
+{
+    namespace TeamB
+    {
+        public class ClassA
+        {
+            public static void DisplayMessage()
+            {
+                Console.WriteLine("Hello from ClassA in ProjectA.Team-B");
+            }
+        }
+    }
+}
