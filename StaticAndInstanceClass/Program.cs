4264429fd3dbdc4b83e754fc4064eb251787c4d2
Dhileeban <berlin4evr@icloud.com>
2025-07-20 01:34:33 +0530

    Static and Instance Class - Example avilable another projects. This will be implemeted later

---
+++ StaticAndInstanceClass/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StaticAndInstanceClass
+{
+    class Circle
+    {
+        float _PI = 3.14f;
+        int _radius;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
