7ff7006a74cfe1dc395a5ce7cedc9311db8c6389
Berlin <berlin4evr@icloud.com>
2025-07-20 00:09:46 +0530

    Read and Write - Console application

---
+++ ReadAndWrite/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ReadAndWrite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + firstName + " " + lastName + "!"); // Output greeting
+
+            Console.WriteLine("Hello {0} {1}!", firstName, lastName); // Output greeting using string interpolation
+
+            Console.WriteLine($"Hello {firstName} {lastName}!"); // Output greeting using string interpolation
+
+        }
+    }
+}
