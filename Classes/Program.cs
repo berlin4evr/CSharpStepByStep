63477c3b89e558e3e18e448e66669be7d3a4d448
Dhileeban <berlin4evr@icloud.com>
2025-07-20 01:29:14 +0530

    Classes - Constructor, Destructor

---
+++ Classes/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Classes
+{
+    class Customer
+    {
+        string _firstName;
+        string _lastName;
+
+        // Constructor - Initializes a new instance of the Customer class
+        public Customer(string firstName, string lastName)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+        }
+
+        public void PrintFullName()
+        {
+            System.Console.WriteLine($"Full Name: {this._firstName} {this._lastName}");
+        }
+
+        ~Customer()
+        {
+            // Destructor - Cleans up resources when the object is no longer needed
+            // This is called when the object is being garbage collected
+            // CLR will call this automatically. No need to call it explicitly.
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer("Dhileeban", "M");
+            customer.PrintFullName();
+        }
+    }
+}
