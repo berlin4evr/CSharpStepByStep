2b8167cbd5d1ada6ab8ee3c2938a5548ad1d33e0
Dhileeban <berlin4evr@icloud.com>
2025-07-20 00:59:13 +0530

    Do-While loop - Example 1

---
+++ DoWhileLoop_1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DoWhileLoop_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter user input");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0; // Initialize a counter variable
+
+            do // Start of the do-while loop
+            {
+                if (i % 2 == 0) 
+                {
+                    Console.WriteLine(i);
+                }
+                i++; // Increment the counter
+            } while (i <= userInput); // Continue looping until the counter exceeds the user input
+        }
+    }
+}
