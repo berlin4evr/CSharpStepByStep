f48383b3deeb7134ffc7b1cfc1d8e9ccd983af47
Dhileeban <berlin4evr@icloud.com>
2025-07-20 00:47:28 +0530

    Switch Statement - Example 1

---
+++ SwitchStatement_1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SwitchStatement_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number between 1 to 15");
+            int number = Convert.ToInt32(Console.ReadLine()); // Reading user input and converting it to an integer
+
+            switch (number)
+            {
+                case 5:
+                    Console.WriteLine($"{number} is equal to 5");
+                    break;
+                case 10:
+                    Console.WriteLine($"{number} is equal to 10");
+                    break;
+                case 15:
+                    Console.WriteLine($"{number} is equal to 15");
+                    break;
+                default:
+                    Console.WriteLine($"{number} is not equal to 5, 10, or 15");
+                    break;
+            }
+
+        }
+    }
+}
